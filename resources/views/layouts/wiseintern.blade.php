8e938de206c3cc3b1c077a90048fe0b00e991935
Bhakti <bhomajbhakti@gmail.com>
2021-04-14 21:05:08 +0530

    added common elements in wiseintern.blade.php

---
+++ resources/views/layouts/wiseintern.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/x-icon" href="http://www.lightcounting.com/build/favicon.ico" />
+    <link rel="stylesheet" href="/css/main.css" />
+    <title>Document</title>
+</head>
+<body class="is-preload homepage">
+    <div id="page-wrapper">
+        @section('header')
+        <div id="header-wrapper">
+            <header id="header" class="container">
+                <!-- Logo -->
+                <div id="logo">
+                    <a href="index.html"><img src="/images/wise-intern-logo.png" width="103" height="49" alt="Wise Intern"/></a>
+                </div>
+                <!-- Nav -->
+                <nav id="nav">
+                    <ul>
+			            <li><a href="projects.html">Projects</a></li>
+                        <li><a href="about.html">About</a></li>
+                        <li><a href="contact.html">Contact</a></li>
+                        <li><a href="#">Login</a></li>
+                        <li><a href="#" class="button-solid">Signup</a></li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+        @show
+
+        <div>@yield('content')</div>
+
+        @section('footer')
+        <div id="footer-wrapper">
+            <footer id="footer" class="container">
+                <div class="row">
+                    <div class="col-12 textcenter">
+                        <section class="widget contact">
+                            <ul>
+                                <li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+                                <li><a href="#" class="icon brands fa-linkedin"><span class="label">LinkedIn</span></a></li>
+                            </ul>
+                            <div id="copyright">
+                                &copy; 2021 Wise Intern. All rights reserved
+                            </div>
+                        </section>
+                    </div>
+                </div>
+            </footer>
+        </div>
+        @show
+    </div>
+    
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -24,5 +24,11 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 Route::get('/usermanagement', function () {
     return view('usermanagement');
 });
+Route::get('/wiseintern', function () {
+    return view('layouts.wiseintern');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
 
 
