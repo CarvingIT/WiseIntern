d2c4c09a92d2e938d0528d5249ec51d8d16e5146
Bhargavi Bindu <bhargavi.bindu2000@gmail.com>
2021-05-12 12:57:10 +0530

    udpated UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,13 +7,8 @@ use App\Models\User;
 
 class UserController extends Controller
 {
-    //
     public function index(){
         $data = User::all();
         return view('/usermanagement',['users'=>$data]);
-       
-        /*return view('/usermanagement');
-        $users = wiseintern::select('select * from users');
-        return view('/admin/usermanagement',['users'=>$users]);*/
         }
 }
