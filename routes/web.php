6f859d5c811f928bd6ba98ac51659dbdc51370fd
truptikulkarni <truptimkulkarni@gmail.com>
2021-05-31 18:35:18 +0530

    web file updated

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
-
+namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,11 +23,9 @@ Route::get('/', function () {
 Route::get('/admin/usermanagement', function () {
     return view('usermanagement');
 })->middleware('admin');
-<<<<<<< HEAD
-=======
 
 Route::get('/admin/usermanagement',[UserController::class,'index'])->middleware('admin');
->>>>>>> main
+
 
 Route::get('/admin/projects', function () {
     return view('projectlists');
